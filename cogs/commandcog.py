25d4be7e5b32121eb8260f756ba65598e0e06de8
AntiMatter-X <51862109+AntiMatter-X@users.noreply.github.com>
2020-09-21 08:06:36 +0900

    Update commandcog.py

--- cogs/commandcog.py
+++ cogs/commandcog.py
@@ -1,5 +1,10 @@
 from discord.ext import commands
 
+from modules import bot_ext
+
+
+be = bot_ext.BotExt()
+
 
 HELPS = {
     "help": ["ヘルプを表示", "コマンドヘルプを表示します。\ncommand引数にコマンド名を指定すると指定したコマンドの詳細が表示されます。", "[command: string]"]
@@ -12,7 +17,7 @@ class CommandCog(commands.Cog):
 
     @commands.command()
     async def help(self, ctx, *args):
-        await ctx.send("pong")
+        await ctx.send(embed=be.embed(["test", "desc"]))
 
 
 def setup(bot):
