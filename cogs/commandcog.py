9d19729c176b1b55b9e17b0f76a2f42ef381aedd
AntiMatter-X <51862109+AntiMatter-X@users.noreply.github.com>
2020-09-20 23:35:12 +0900

    Update commandcog.py

--- cogs/commandcog.py
+++ cogs/commandcog.py
@@ -9,6 +9,10 @@ class CommandCog(commands.Cog):
     async def ping(self,ctx):
         await ctx.send("pong")
 
+    @commands.command()
+    async def pien(self,ctx):
+        await ctx.send("ぱおん")
+
 
 def setup(bot):
     bot.add_cog(CommandCog(bot))
