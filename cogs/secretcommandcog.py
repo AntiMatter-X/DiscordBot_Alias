749cc47e19550e3e9b4209a9d8230f92bdf57553
AntiMatter-X <51862109+AntiMatter-X@users.noreply.github.com>
2020-09-21 15:29:19 +0900

    Update secretcommandcog.py

--- cogs/secretcommandcog.py
+++ cogs/secretcommandcog.py
@@ -19,9 +19,14 @@ class SecretCommandCog(commands.Cog):
             "\U0001F97A",
             "\U0001F418"
         ]
-        type = random.randint(0, 2)
+        type = random.randint(0, 3)
         if type == 0: await ctx.message.add_reaction(random.choice(emoji))
         elif type == 1: await ctx.send(random.choice(emoji))
+        elif type == 2:
+            for e in [
+                ["\U0001F1F5", "\U0001F1EE", "\U0001F1EA", "\U0001F1F3"],
+                ["\U0001F1F5", "\U0001F1E6", "\U0001F1F4", "\U0001F1F3"]
+             ]: await ctx.message.add_reaction(e)
         else: await ctx.send(random.choice([
             "ぴえん",
             "ぱおん"
