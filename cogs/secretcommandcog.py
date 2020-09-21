24622831739b6fcaf94c4a78d875b7aabe38e3ce
AntiMatter-X <51862109+AntiMatter-X@users.noreply.github.com>
2020-09-21 14:53:50 +0900

    Update secretcommandcog.py

--- cogs/secretcommandcog.py
+++ cogs/secretcommandcog.py
@@ -1,1 +1,21 @@
+from discord.ext import commands
 
+from modules import bot_ext
+
+
+be = bot_ext.BotExt()
+
+
+class SecretCommandCog(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    # pien -----
+    @commands.command()
+    async def pien(self, ctx):
+        await ctx.message.add_reaction("\U0001F97A")
+    #-----------
+
+
+def setup(bot):
+    bot.add_cog(SecretCommandCog(bot))
