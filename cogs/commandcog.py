60a3fd9b1ec426f18f8f893846c834332e026ac7
AntiMatter-X <51862109+AntiMatter-X@users.noreply.github.com>
2020-09-21 11:28:38 +0900

    Update commandcog.py

--- cogs/commandcog.py
+++ cogs/commandcog.py
@@ -25,6 +25,10 @@ class CommandCog(commands.Cog):
 
         else: await ctx.send(embed=be.embed(["コマンドヘルプ", f'**`プレイフィックス: "{os.environ["DISCORD_BOT_PREFIX"]}"`**'], fields=[[k, HELPS[k][0], True] for k in HELPS]))
 
+     @commands.command(name="bot")
+     async def bot_(self, ctx):
+         await ctx.send("abc")
+
 
 def setup(bot):
     bot.add_cog(CommandCog(bot))
