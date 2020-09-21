8da1643a94161c22fd17c7ff80b32a4b17e07a6f
AntiMatter-X <51862109+AntiMatter-X@users.noreply.github.com>
2020-09-21 15:13:40 +0900

    Update secretcommandcog.py

--- cogs/secretcommandcog.py
+++ cogs/secretcommandcog.py
@@ -1,3 +1,5 @@
+import random
+
 from discord.ext import commands
 
 from modules import bot_ext
@@ -13,7 +15,10 @@ class SecretCommandCog(commands.Cog):
     # pien -----
     @commands.command()
     async def pien(self, ctx):
-        await ctx.message.add_reaction("\U0001F97A")
+        type = random.randint(0, 2)
+        if type == 0: await ctx.message.add_reaction("\U0001F97A")
+        elif type == 1: await ctx.send("\U0001F97A")
+        else: await ctx.send("ぱおん")
     #-----------
 
 
