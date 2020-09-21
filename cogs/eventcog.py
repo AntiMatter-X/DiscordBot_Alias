a99594177ed15ae0c97a292fa107744d7d6686a8
AntiMatter-X <51862109+AntiMatter-X@users.noreply.github.com>
2020-09-21 11:01:37 +0900

    Update eventcog.py

--- cogs/eventcog.py
+++ cogs/eventcog.py
@@ -1,1 +1,38 @@
+import os
+import discord
+import datetime
 
+from discord.ext import commands
+
+
+async def refresh_act(bot):
+    await bot.change_presence(activity=discord.Game(name=" | ".join([
+        f"{os.environ['DISCORD_BOT_PREFIX']}help",
+        f"{len(bot.guilds)} servers",
+        f"{sum([g.member_count for g in bot.guilds])} members",
+        f"refresh date: {'{0:%Y-%m-%d %H:%M:%S}'.format(datetime.datetime.now(datetime.timezone(datetime.timedelta(hours=+9), 'JST')))}"
+    ])))
+
+
+class EventCog(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_ready(self): refresh_act(self.bot)
+
+    @commands.Cog.listener()
+    async def on_guild_join(self, guild): refresh_act(self.bot)
+
+    @commands.Cog.listener()
+    async def on_guild_remove(self, guild): refresh_act(self.bot)
+
+    @commands.Cog.listener()
+    async def on_member_join(self, member): refresh_act(self.bot)
+
+    @commands.Cog.listener
+    async def on_member_remove(self, member): refresh_act(self.bot)
+
+
+def setup(bot):
+    bot.add_cog(EventCog(bot))
