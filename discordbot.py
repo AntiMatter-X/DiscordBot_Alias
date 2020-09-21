1ed7d41a40371a160dcade692a2d3677c8aee28d
AntiMatter-X <51862109+AntiMatter-X@users.noreply.github.com>
2020-09-21 14:22:04 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -11,13 +11,13 @@ INITIAL_EXTENSIONS = [
 
 
 class MyBot(commands.Bot):
-    def __init__(self, command_prefix, help_command):
-        super().__init__(command_prefix, help_command=help_command)
+    def __init__(self, command_prefix, help_command, case_insensitive):
+        super().__init__(command_prefix, help_command=help_command, case_insensitive=case_insensitive)
         for cog in INITIAL_EXTENSIONS:
             try: self.load_extension(cog)
             except Exception: traceback.print_exc()
 
 
 if __name__ == "__main__":
-    bot = MyBot(command_prefix=os.environ["DISCORD_BOT_PREFIX"], help_command=None)
+    bot = MyBot(command_prefix=os.environ["DISCORD_BOT_PREFIX"], help_command=None, case_insensitive=True)
     bot.run(os.environ["DISCORD_BOT_TOKEN"])
