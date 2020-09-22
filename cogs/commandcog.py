b24dd09731cef0222c3931ed590934c0fc9cebfa
AntiMatter-X <51862109+AntiMatter-X@users.noreply.github.com>
2020-09-22 09:59:18 +0900

    Update commandcog.py

--- cogs/commandcog.py
+++ cogs/commandcog.py
@@ -54,6 +54,7 @@ class CommandCog(commands.Cog):
     @b_o_t_.command()
     async def ping(self, ctx):
         await ctx.send(f"{round(self.bot.latency * 1000, 1)}ms")
+        await ctx.send(f"{round(self.bot.latency * 1000)}ms")
     #----------
 
 
